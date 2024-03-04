f0da9e1d52bf77791d97cae8d2a257771b4b7afd
maybe-skiFF <den.skiff190@gmail.com>
2024-03-04 21:28:57 +0300

    feat: add solution removeProperties task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -51,8 +51,12 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const resObj = obj;
+  keys.forEach((item) => {
+    delete resObj[item];
+  });
+  return resObj;
 }
 
 /**
